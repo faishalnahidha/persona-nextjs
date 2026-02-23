6ebc21786634650ba723191fea42196dfe3ecbc7
DESKTOP-6ACEH2V\ialda <yugiholic@gmail.com>
2026-02-23 13:33:32 +0700

    feat: refactor homepage the left side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import {
   IconFlareFilled,
 } from "@tabler/icons-react";
 
+import { Avatar, AvatarFallback, AvatarGroup, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Header from "@/components/header";
@@ -15,107 +16,127 @@ export default function HomePage() {
     <main className="min-h-screen">
       <Header />
 
-      <section className="w-full h-400 pt-18 bg-linear-to-r from-brand-neutral-100 to-brand-neutral-50">
-        <div className="container mx-auto w-full items-center grid grid-cols-[3fr_2fr] gap-10 px-6 md:px-10">
+      <section className="w-full h-screen bg-linear-to-r from-brand-neutral-100 to-brand-neutral-50">
+        <div className="container mx-auto w-full h-full items-center grid grid-cols-1 lg:grid-cols-[3fr_2fr] gap-4 md:gap-6 lg:gap-10 px-6 md:px-10">
+
+          {/* Left Side */}
           <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-6">
-              <h1 className="text-6xl font-medium text-[#27272a]">
-                Kenali dirimu untuk menemukan pekerjaan terbaik
+              <h1 className="text-6xl font-medium text-foreground">
+                Kenali diri untuk temukan pekerjaan terbaik
               </h1>
-              <p className="para-lg text-[#3f3f46]">
-                Tes kepribadian MBTI online &amp; panduan potensi diri dalam
+              <p className="para-lg text-foreground-alt">
+                Tes kepribadian MBTI <span className="italic">online</span> &amp; panduan potensi diri dalam
                 mencapai karir terbaik
               </p>
             </div>
 
-            <div className="grid w-fit grid-cols-3 gap-2">
-              <div className="h-[196px] w-[232px] rounded-2xl bg-[#71717a] p-4">
-                <div className="flex items-center gap-2">
-                  <div className="flex -space-x-2">
-                    <div className="size-10 rounded-full bg-[#d4d4d8] ring-2 ring-[#71717a]" />
-                    <div className="size-10 rounded-full bg-[#a1a1aa] ring-2 ring-[#71717a]" />
-                    <div className="size-10 rounded-full bg-[#e4e4e7] ring-2 ring-[#71717a]" />
-                    <div className="size-10 rounded-full bg-[#d4d4d8] ring-2 ring-[#71717a]" />
-                  </div>
-                </div>
+            {/* Cards for illustration*/}
+            <div className="grid grid-cols-3 gap-2 min-h-48 py-4 pr-0 2xl:pr-20">
+              {/* Cards #1*/}
+              <div className="w-full h-full rounded-2xl bg-brand-neutral-500 p-4">
+                <div className="flex flex-col h-full justify-between gap-4">
+                  <div className="flex flex-col gap-3">
+                    <AvatarGroup className="*:data-[slot=avatar]:ring-brand-neutral-500">
+                      <Avatar className="size-12 bg-brand-neutral-600">
+                        <AvatarImage src="/images/avatar-tp/avatar-tp-enfp.webp" alt="ENFP" />
+                        <AvatarFallback className="bg-brand-neutral-600" />
+                      </Avatar>
+                      <Avatar className="size-12  bg-brand-neutral-400">
+                        <AvatarImage src="/images/avatar-tp/avatar-tp-entj.webp" alt="ENTJ" />
+                        <AvatarFallback className="bg-brand-neutral-400" />
+                      </Avatar>
+                      <Avatar className="size-12 bg-brand-neutral-600">
+                        <AvatarImage src="/images/avatar-tp/avatar-tp-esfp.webp" alt="ESFP" />
+                        <AvatarFallback className="bg-brand-neutral-600" />
+                      </Avatar>
+                      <Avatar className="size-12 bg-brand-neutral-400">
+                        <AvatarImage src="/images/avatar-tp/avatar-tp-isfj.webp" alt="ISFJ" />
+                        <AvatarFallback className="bg-brand-neutral-400" />
+                      </Avatar>
+                    </AvatarGroup>
 
-                <div className="mt-10 flex items-start gap-[6px]">
-                  <div className="text-4xl font-medium font-heading text-[#f4f4f5]">
-                    16
+                    <div className="flex items-center gap-2 ">
+                      <div className="text-4xl/11 font-medium font-heading text-brand-neutral-100">
+                        16
+                      </div>
+                      <div className="whitespace-pre-line para-sm-medium text-brand-neutral-100">
+                        tipe{"\n"}kepribadian
+                      </div>
+                    </div>
                   </div>
-                  <div className="whitespace-pre-line text-sm font-medium tracking-[-0.1px] text-[#f4f4f5]">
-                    tipe{"\n"}kepribadian
-                  </div>
-                </div>
 
-                <div className="mt-6 flex items-center gap-1.5">
-                  <div className="text-heading-display-xs text-[#d4d4d8]">
-                    100+
-                  </div>
-                  <div className="text-body-sm-medium leading-none text-[#d4d4d8]">
-                    artikel panduan
+                  <div className="flex items-center gap-2">
+                    <div className="text-xl font-medium font-heading text-brand-neutral-300">
+                      100+
+                    </div>
+                    <div className="para-sm-medium text-brand-neutral-300">
+                      artikel panduan
+                    </div>
                   </div>
                 </div>
               </div>
 
-              <div className="h-[196px] w-[232px] rounded-2xl bg-[#a1a1aa] p-4">
+              {/* Cards #2*/}
+              <div className="w-full h-full rounded-2xl bg-brand-neutral-400 p-4">
                 <div className="flex items-center justify-between">
-                  <div className="text-body-lg-medium text-[#f4f4f5]">
+                  <div className="text-lg font-medium text-brand-neutral-100">
                     Hasil tes
                   </div>
-                  <div className="flex items-center gap-1 text-[#f4f4f5]">
-                    <IconFlareFilled className="size-5 opacity-70" />
-                    <IconFlareFilled className="size-5 opacity-40" />
+                  <div className="flex items-center gap-1">
+                    <IconFlareFilled className="size-5 text-brand-neutral-300" />
+                    <IconFlareFilled className="size-5 text-brand-neutral-300" />
                   </div>
                 </div>
 
-                <div className="mt-4 space-y-3">
-                  <div className="flex items-center justify-between text-mono-sm text-[#d4d4d8]">
-                    <span>20%</span>
-                    <span className="text-[#e4e4e7]">80%</span>
+                <div className="mt-4 space-y-1">
+                  <div className="flex h-8 overflow-hidden rounded-full border-2 border-brand-neutral-600">
+                    <div className="flex w-[30%] items-center px-3 bg-transparent">
+                      <span className="text-sm font-medium font-mono text-brand-neutral-200">30%</span>
+                    </div>
+                    <div className="flex flex-1 items-center justify-end px-3 bg-brand-neutral-600">
+                      <span className="text-sm font-medium font-mono text-brand-neutral-200">70%</span>
+                    </div>
                   </div>
-                  <div className="h-2 w-full rounded-full bg-[#d4d4d8]">
-                    <div className="h-2 w-[80%] rounded-full bg-[#71717a]" />
-                  </div>
-                  <div className="flex items-center justify-between text-body-xs text-[#e4e4e7]">
+                  <div className="flex items-center justify-between px-1 text-[0.625rem] text-brand-neutral-300">
                     <span>Extroverted</span>
                     <span>Introverted</span>
                   </div>
                 </div>
 
-                <div className="mt-4 space-y-3">
-                  <div className="flex items-center justify-between text-mono-sm text-[#d4d4d8]">
-                    <span>55%</span>
-                    <span className="text-[#e4e4e7]">45%</span>
+                <div className="mt-4 space-y-1">
+                  <div className="flex h-8 overflow-hidden rounded-full border-2 border-brand-neutral-600">
+                    <div className="flex w-[55%] items-center px-3 bg-brand-neutral-600">
+                      <span className="text-sm font-medium text-brand-neutral-200">55%</span>
+                    </div>
+                    <div className="flex flex-1 items-center justify-end px-3 bg-transparent">
+                      <span className="text-sm font-medium text-brand-neutral-200">45%</span>
+                    </div>
                   </div>
-                  <div className="h-2 w-full rounded-full bg-[#d4d4d8]">
-                    <div className="h-2 w-[55%] rounded-full bg-[#71717a]" />
-                  </div>
-                  <div className="flex items-center justify-between text-body-xs text-[#e4e4e7]">
+                  <div className="flex items-center justify-between px-1 text-[0.625rem] text-brand-neutral-300">
                     <span>Sensory</span>
                     <span>Intuitive</span>
                   </div>
                 </div>
               </div>
-
-              <div className="h-[196px] w-[232px] rounded-2xl bg-[#e4e4e7] p-4">
-                <div className="flex items-start justify-between">
-                  <div>
-                    <div className="text-heading-display-md text-[#71717a]">
-                      500+
-                    </div>
-                    <div className="whitespace-pre-line text-body-sm-medium text-[#71717a]">
-                      tes telah{"\n"}dikerjakan
-                    </div>
+              {/* Cards #3*/}
+              <div className="w-full h-full rounded-2xl bg-brand-neutral-200 p-4 flex flex-col">
+                <div>
+                  <div className="text-5xl font-heading font-medium text-brand-neutral-500">
+                    500+
                   </div>
-                  <div className="rounded-lg bg-[#d4d4d8] p-2">
-                    <IconFileTextSpark className="size-6 text-[#71717a]" />
+                  <div className="whitespace-pre-line w-30 my-1 text-sm font-medium text-brand-neutral-500">
+                    tes telah dikerjakan
                   </div>
+                </div>
+                <div className="flex flex-1 items-end justify-end">
+                  <IconFileTextSpark className="size-16 text-brand-neutral-400" />
                 </div>
               </div>
             </div>
           </div>
 
+          {/* Right Side */}
           <div className="pl-6">
             <div className="h-[530px] w-[480px] rounded-2xl bg-[#27272a] p-10">
               <div className="flex w-[400px] flex-col gap-10">
